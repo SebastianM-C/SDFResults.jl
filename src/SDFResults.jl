46eec840d9a9e9159e6a0b32b0e637574b86edfd
Sebastian Micluța-Câmpeanu <m.c.sebastian95@gmail.com>
2021-01-19 15:41:00 +0200

    Add code

--- src/SDFResults.jl
+++ src/SDFResults.jl
@@ -1,5 +1,22 @@
 module SDFResults
 
-# Write your package code here.
+export read_simulation, EPOCHSimulation, SDFFile,
+    get_time, get_parameter, get_npart
+
+using Unitful
+using PhysicalConstants.CODATA2018: c_0, ε_0, μ_0, m_e, e
+using Statistics, StatsBase
+using BangBang
+using Transducers, ThreadsX
+using SDFReader
+using SDFReader: get_units
+using PICDataStructures
+using EPOCHInput
+
+include("files.jl")
+include("simulation.jl")
+include("traits.jl")
+include("read.jl")
+include("utils.jl")
 
 end
