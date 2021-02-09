0c931658cc4ba0c9fbe10e799172513e49e07ba7
Documenter.jl <documenter@juliadocs.github.io>
2021-02-09 14:49:27 +0000

    build based on f4d5fd1

---
+++ v0.1.1/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = SDFResults","category":"page"},{"location":"#SDFResults","page":"Home","title":"SDFResults","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [SDFResults]","category":"page"}]
+}
