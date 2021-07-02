0e18b5483dbf561e6658c5bb7636f4b01f0304c0
Sebastian Micluța-Câmpeanu <m.c.sebastian95@gmail.com>
2021-07-02 18:06:50 +0300

    Relax type constraints

--- src/utils.jl
+++ src/utils.jl
@@ -15,3 +15,48 @@ end
 
 get_parameter(sim::EPOCHSimulation, p::Symbol) = getindex(sim.param, p)
 get_parameter(sim::EPOCHSimulation, p::Symbol, c::Symbol) = getindex(get_parameter(sim, p), c)
+
+function domain_length(sdf, dir::Symbol)
+    d_min = Symbol(string(dir)*"_min")
+    d_max = Symbol(string(dir)*"_max")
+
+    get_parameter(sdf, d_max) - get_parameter(sdf, d_min)
+end
+
+function domain_volume(sdf)
+    nd = ndims(sdf)
+
+    Δx = domain_length(sdf, :x)
+
+    if ndims == 1
+        Δx
+    elseif ndims == 2
+        Δy = domain_length(sdf, :y)
+        Δx * Δy
+    else
+        Δy = domain_length(sdf, :y)
+        Δz = domain_length(sdf, :z)
+        Δx * Δy * Δz
+    end
+end
+
+function cell_volume(sdf)
+    nd = ndims(sdf)
+    vol = domain_volume(sdf)
+    nx = get_parameter(sdf, :nx)
+
+    if ndims == 1
+        vol / nx
+    elseif ndims == 2
+        ny = get_parameter(sdf, :ny)
+        vol / (nx * ny)
+    else
+        ny = get_parameter(sdf, :ny)
+        nz = get_parameter(sdf, :nz)
+        vol / (nx * ny * nz)
+    end
+end
+
+function cell_length(sdf, direction::Symbol)
+    domain_length(sdf, direction)/get_parameter(sdf, Symbol("n"*"$direction"))
+end
